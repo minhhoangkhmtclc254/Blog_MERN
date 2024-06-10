02a8a7b4fdaff0e8701db0831c4b49c4afd3f25c
nminh <minhlui254qn254@gmail.com>
2024-06-10 11:25:59 +0700

    redux

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -27,6 +27,7 @@ export default function SignUp() {
       });
       const data = await res.json();
       if (data.success === false) {
+        setLoading(false);
         return setErrorMessage(data.message);
       }
       setLoading(false);
